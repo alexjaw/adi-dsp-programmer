f65afac6249c180dd3d8aaf93c622b69f42f8085
Alexander Jaworowski <alexander@jaworowski.se>
2020-02-19 16:46:57 +0100

    Some updates

--- download.c
+++ download.c
@@ -3,8 +3,8 @@
 //
 
 #include "download.h"
-#include "system_files/Example-bap2-spdif-xmos_IC_1.h"
-#include "system_files/Example-bap2-spdif-xmos_IC_2.h"
+#include "system_files/Example-bap2-spdif-xmos-2_IC_1.h"
+#include "system_files/Example-bap2-spdif-xmos-2_IC_2.h"
 
 void download(){
     default_download_IC_1();
--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 #include <math.h>
 #include "i2c.h"
 #include "download.h"
-#include "vol.h"
 
 #define ARG_RW       1  // index in the arguments list
 #define ARG_ADDR8    2
@@ -18,20 +17,65 @@ unsigned char addr8  = 0;  // 8-bit i2c addr for read
 unsigned int reg     = 0;  // dsp reg addr
 unsigned int n_bytes = 0;  // number of bytes to read/write
 
+#define F824_ONE   0x01000000  //32-bit
+#define F824_SIGN  0xF0000000  //-16 in 8.24
+#define F824_M60DB_MASK 0xFFFFC00  // todo: AJ what should this be for 8.24???
+//adau1452
+static float conv523toFloat(int i523) {
+    float dec;
+    // sign extend
+    if ( i523 & 0x8000000 )  //#define F523_SIGN  0x8000000  //-16 in 5.23
+        i523 |= 0xF0000000; // sign extend
+    // and devide
+    dec =  (float)i523/(float)(1<<23);
+    //DBGMSG(LOG_INFO,"0x%08x (8.24: 0x%08x) = %f\n",i523,convFloatTo824(dec),dec);
+    return dec;
+}
+static int convFloatTo523(float dec) {
+    int t523;
+    t523 = (dec * (float)(1<<23));
+    // sign shorten
+    t523 &= 0xFFFFFFFF;
+    //DBGMSG(LOG_INFO,"%f = 0x%08x (8.24: 0x%08x)\n",dec,t523, convFloatTo824(dec));
+    return t523;
+}
+
+//adau1467
+float conv824toFloat(int i824){
+    float dec;
+    // sign extend
+    if ( i824 & F824_SIGN )
+        i824 |= 0xF0000000; // sign extend
+    // and devide
+    dec =  (float)i824/(float)(1<<24);
+    printf("0x%08x (5.23: 0x%08x) = %f\n",i824,convFloatTo523(dec),dec);
+    return dec;
+}
+int convFloatTo824(float dec){
+    int t824;
+    t824 = (dec * (float)(1<<24));
+    // sign shorten
+    t824 &= 0x0FFFFFFF;
+    printf("%f = 0x%08x (5.23: 0x%08x)\n",dec,t824, convFloatTo523(dec));
+    return t824;
+}
+
 void dec2hex(double x, unsigned char buf[], int buf_sz){
-    unsigned int r;
+    unsigned int r, r523;
     int a = 8, b = 24;
 
     if (x >= 0) {
         r = (unsigned int)round(pow(2, b) * x);
+        r523 = (unsigned int)round(pow(2, 23) * x);
     }else{
         r = (unsigned int)round(pow(2, a+b) - (pow(2, b) * fabs(x)));
+        r523 = (unsigned int)round(pow(2, 5+23) - (pow(2, 23) * fabs(x)));
     }
     buf[0] = r >> 24;
     buf[1] = r >> 16;
     buf[2] = r >> 8;
     buf[3] = r;
-    printf("dec2hex(%f) in 8.24: %i, 0x%08x\n", (float)x, r, r);
+    printf("dec2hex(%f) in 8.24: %i, 0x%08x. In 5.23: %i, 0x%08x\n", (float)x, r, r, r523, r523);
     //printf("dec2hex: 0x%02x 0x%02x 0x%02x 0x%02x\n", buf[0], buf[1], buf[2], buf[3]);
 }
 
@@ -86,11 +130,12 @@ int main(int argc, char *argv[]) {
         if(argc == 6){
 #define BUF_BYTES       4
 #define ALPHA_REG_BYTES 8
-            //n_bytes = 4;
             int err = 0;
             float vol = 0.0, g = 0.0;
-            unsigned short alpha_reg = 0x0055;
-            unsigned char alpha_reg_data[ALPHA_REG_BYTES] = {0x00, 0xff, 0xf9, 0x2c, 0x00, 0x00, 0x06, 0xd4};
+            unsigned short gain_reg = 0x006d;
+            printf("using fixed gain adj reg: 0x%04x\n", gain_reg);
+            unsigned short alpha_reg = 0x006e;
+            unsigned char alpha_reg_data[ALPHA_REG_BYTES] = {0x00, 0xff, 0xfb, 0xd5, 0x00, 0x00, 0x04, 0x2b};
             sscanf(argv[ARG_VOL], "%f", &vol);
             if(vol < 0 || vol > 100){
                 printf("ERROR. vol: 0 <= vol <= 100\n");
@@ -105,9 +150,11 @@ int main(int argc, char *argv[]) {
                 return -1;
             }
             printf("gain2bytes: 0x%02x 0x%02x 0x%02x 0x%02x\n", buf[0], buf[1], buf[2], buf[3]);
+            //printf("OBSERVE, skipping i2c execution of command\n");
+            //return 0;
             i2cOpen();
             addr8 = addr8>>1;
-            err = write_i2c_block_data(addr8, reg, buf, BUF_BYTES);
+            err = write_i2c_block_data(addr8, gain_reg, buf, BUF_BYTES);
             if(err){
                 printf("Failed to set gain\n");
             }
